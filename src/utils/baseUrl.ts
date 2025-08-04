f12feacf154d8dcd54c958123cc0463a13f7583c
alirezashn79 <alireza.sharifinasabb@yahoo.com>
2025-08-04 19:29:24 +0330

    fix(front): base url in production

--- src/utils/baseUrl.ts
+++ src/utils/baseUrl.ts
@@ -1,4 +1,3 @@
-const baseURL =
-  process.env.NODE_ENV === 'production' ? 'https://todox.liara.run' : 'http://localhost:3000'
+const baseURL = process.env.NODE_ENV === 'production' ? 'https://todox.ir' : 'http://localhost:3000'
 
 export default baseURL
